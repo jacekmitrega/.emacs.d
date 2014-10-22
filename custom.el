95140fab1c435fce6757b77194122bf1db3dd75a
Jacek Mitręga <jacek.mitrega@gmail.com>
2014-10-22 17:44:01 +0200

    custom.el with initial customizations

---
+++ custom.el
@@ -0,0 +1,37 @@
+;;; custom.el
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(apropos-do-all t)
+ '(blink-cursor-delay 0.3)
+ '(blink-cursor-interval 0.3)
+ '(column-number-mode t)
+ '(global-auto-revert-mode t)
+ '(indicate-empty-lines t)
+ '(inhibit-startup-echo-area-message "ramirez")
+ '(inhibit-startup-screen t)
+ '(initial-scratch-message nil)
+ '(isearch-allow-scroll t)
+ '(kill-do-not-save-duplicates t)
+ '(lazy-highlight-initial-delay 0.125)
+ '(mouse-yank-at-point t)
+ '(ns-use-native-fullscreen t)
+ '(save-interprogram-paste-before-kill t)
+ '(scroll-preserve-screen-position 1)
+ '(show-paren-mode t)
+ '(show-paren-style (quote parenthesis))
+ '(size-indication-mode t)
+ '(tmm-completion-prompt nil)
+ '(x-select-enable-clipboard t)
+ '(x-select-enable-primary t)
+ '(yank-pop-change-selection t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,5 @@
+;;; init.el
+
+;;; Custom
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file)
