e80b924715428cf33733052b22cbfdf02ff238a8
Jacek Mitręga <jacek.mitrega@gmail.com>
2014-12-22 11:30:45 +0100

    Disable scroll-bar-mode

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
 (if window-system
   (progn
     (if (fboundp 'menu-bar-mode) (menu-bar-mode 1))
-    (if (fboundp 'scroll-bar-mode) (scroll-bar-mode 1))
+    (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
     )
   (progn
     (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
